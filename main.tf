fdedb8850728327f513a1be341e4b83cf3dabe50
Zack Davenport <zackdavenport@tcadmins-MacBook-Air.local>
2020-09-02 13:42:29 +0100

    changes made to the gitignore and main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #==================================main tier====================================
 
-provider "AWS" {
+provider "aws" {
   region =  "eu-west-2"
 }
 
@@ -13,4 +13,18 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource route_table_id
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [aws_vpc.main.id]
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = data.aws_internet_gateway.default.id
+  }
+}
