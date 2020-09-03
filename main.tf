dc4acb24713e83b2ec026ab4c494ff84cfb333be
Zack Davenport <zackdavenport@tcadmins-MacBook-Air.local>
2020-09-03 13:04:40 +0100

    vpc, igw, routetb and subnet added

--- main.tf
+++ main.tf
@@ -12,19 +12,33 @@ resource "aws_vpc" "main" {
     Name = "main_vpc"
   }
 }
+#====================================== igw ====================================
 
-data "aws_internet_gateway" "default" {
-  filter {
-    name   = "attachment.vpc-id"
-    values = [aws_vpc.main.id]
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-ig"
   }
 }
+#==================================== routetable ===============================
 
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = data.aws_internet_gateway.default.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+}
+
+#==================================== publicsubnet =============================
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main-public-subnet"
   }
 }
